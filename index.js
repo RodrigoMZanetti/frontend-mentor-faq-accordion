2d5560d1d0533070af6d8f65dcf5bf821f093177
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-03-26 13:59:47 -0300

    feat: creates background image

---
+++ index.js
@@ -0,0 +1,23 @@
+const faqOpenButtons = document.querySelectorAll(".accordion-header");
+
+faqOpenButtons.forEach((button) => {
+  button.addEventListener("click", (evt) => {
+    const clickedButton = evt.currentTarget;
+    const mainAccordion = clickedButton.closest(".accordion");
+    const containerTarget = mainAccordion.querySelector(
+      ".accordion__container",
+    );
+    containerTarget.classList.toggle("accordion-is-open");
+
+    console.log("cliquei");
+    console.log(clickedButton);
+    console.log(mainAccordion);
+    console.log(containerTarget);
+
+    if (containerTarget.classList.contains("accordion-is-open")) {
+      clickedButton.setAttribute("aria-expanded", "true");
+    } else {
+      clickedButton.setAttribute("aria-expanded", "false");
+    }
+  });
+});
